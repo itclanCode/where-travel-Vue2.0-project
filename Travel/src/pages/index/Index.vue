3b7e353624d8970b3eb03539665feeed35c72688
itclancode <itclancode@163.com>
2018-08-19 17:29:57 +0800

    add header banner static

---
+++ Travel/src/pages/index/Index.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+          <index-header></index-header>
+          <index-banner></index-banner>  
+    </div>
+</template>
+
+<script>
+    import IndexHeader from './components/Header' // 头部header组件
+    import IndexBanner from './components/Banner' // banner组件
+    export default {
+        name: "Index",
+        components: {
+            IndexHeader,
+            IndexBanner
+        }
+    }
+</script>
+
+<style lang="stylus" scoped>
+ 
+</style>
